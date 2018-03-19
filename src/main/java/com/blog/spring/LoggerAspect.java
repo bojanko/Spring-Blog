c78cebd75e5454be42edb2c2f4da2d898b7cd38b
bojanko <31866364+bojanko@users.noreply.github.com>
2018-03-19 23:45:28 +0100

    Logovanje pristupa stranicama
    Kreiranje aspekta koji loguje svaki pristup stranici

---
+++ src/main/java/com/blog/spring/LoggerAspect.java
@@ -0,0 +1,20 @@
+package com.blog.spring;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component 
+public class LoggerAspect {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Before(value="execution(* com.blog.spring.HomeController.*(..))", argNames = "joinPoint")
+	public void logger(JoinPoint joinPoint){
+		logger.info("Welcome to "+joinPoint.getSignature().getName()+" page!");
+	}
+}
